e3d9b24700582e9fafe867589d04cc2023dafe51
Charles Lechasseur <shiftingbeard@gmx.com>
2023-09-11 04:18:46 -0400

    Add doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,148 @@
-//! # mini_exercism
+//! A lightweight crate to interact with the [Exercism website](https://exercism.org)'s APIs.
 //!
-//! A lightweight crate to interact with [Exercism](https://exercism.org)'s website API.
+//! # Installing
+//!
+//! Add [mini_exercism](crate) to your dependencies:
+//!
+//! ```toml
+//! [dependencies]
+//! mini_exercism = "0"
+//! ```
+//!
+//! or by running:
+//!
+//! ```bash
+//! cargo add mini_exercism
+//! ```
+//!
+//! # API clients
+//!
+//! To interact with an [Exercism](https://exericms.org) API, you can simply use one of
+//! the provided API clients. Each API has its own client:
+//!
+//! - [`api::v1::Client`]
+//! - [`api::v2::Client`]
+//!
+//! # Async methods
+//!
+//! The client methods used to query the APIs for information are `async`. As such, in order to
+//! call them, you will need to use the `await` keyword. For more information on async programming
+//! in Rust, see [Asynchronous Programming in Rust](https://rust-lang.github.io/async-book/).
+//!
+//! Asynchronous programming in Rust requires an _async runtime_. Runtimes handle the execution
+//! of asynchronous code and the callbacks.
+//!
+//! One popular async runtime is [Tokio](https://tokio.rs/). It offers a simple way to write
+//! programs that support asynchronous code. You can use the [`#[tokio::main]`](https://docs.rs/tokio/latest/tokio/attr.main.html)
+//! attribute to make your `main` function `async`, allowing the use of `await`:
+//!
+//! ```no_run
+//! use mini_exercism::api;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let client = api::v2::Client::new();
+//!     let tracks = client.get_tracks(None).await;
+//!     // ...
+//! }
+//! ```
+//!
+//! [Tokio](https://tokio.rs/) offers many customization options; see the [docs](https://docs.rs/tokio/latest/tokio/index.html)
+//! for more details.
+//!
+//! # Example: fetching language tracks
+//!
+//! ```no_run
+//! use mini_exercism::api;
+//!
+//! # futures::executor::block_on(async {
+//! let client = api::v2::Client::new();
+//! let tracks = client.get_tracks(None).await.unwrap().tracks;
+//! for track in &tracks {
+//!     println!("Exercism language track: {}", track.title);
+//! }
+//! # });
+//! ```
+//!
+//! # Credentials
+//!
+//! API clients use [`Credentials`] to perform requests as a specific user. Some requests can work
+//! both anonymously and authenticated (behaving differently depending on which is used), others
+//! require authentication to work.
+//!
+//! [`Credentials`] use [Exercism](https://exercism.org) API tokens to identify a user. This is the
+//! token that is used for the [Exercism CLI application](https://exercism.org/docs/using/solving-exercises/working-locally).
+//! It can be fetched from the [Exercism's user settings page](https://exercism.org/settings/api_cli).
+//!
+//! To pass [`Credentials`] to an API client, use its `builder`:
+//!
+//! ```no_run
+//! use mini_exercism::api;
+//! use mini_exercism::core::Credentials;
+//!
+//! let credentials = Credentials::from_api_token("SOME_API_TOKEN");
+//! let client = api::v2::Client::builder()
+//!     .credentials(credentials)
+//!     .build();
+//! // From here on, `client` will perform requests as the authenticated user.
+//! ```
+//!
+//! # Fetching CLI credentials
+//!
+//! This crate provides a helper function to fetch the [`Credentials`] used by the currently-installed
+//! [Exercism CLI application](https://exercism.org/docs/using/solving-exercises/working-locally).
+//! In order to use this, you need to enable the `cli` feature:
+//!
+//! ```toml
+//! [dependencies]
+//! mini_exercism = { version = "0", features = ["cli"] }
+//! ```
+//!
+//! Then, you can fetch CLI credentials and use them to perform API requests. Note that it's
+//! possible for the method to fail to find credentials if the CLI is not installed, for instance.
+//!
+//! ```no_run
+//! use mini_exercism::api;
+//! use mini_exercism::cli::get_cli_credentials;
+//!
+//! let mut client_builder = api::v2::Client::builder();
+//! let cli_credentials = get_cli_credentials();
+//! if let Ok(credentials) = cli_credentials {
+//!     client_builder.credentials(credentials);
+//! } else {
+//!     // Find some other way to fetch credentials, or perform queries anonymously
+//! }
+//! let client = client_builder.build();
+//! ```
+//!
+//! # Customizing the HTTP client
+//!
+//! Internally, [`mini_exercism`](crate) uses the [`reqwest`](https://crates.io/crates/reqwest)
+//! library to perform HTTP calls. Unless overridden, API clients will create a default HTTP client.
+//! If you need to customize the behavior of the HTTP client, you can use the API client's `builder`
+//! to specify a different HTTP client:
+//!
+//! ```no_run
+//! use mini_exercism::api;
+//! let http_client_builder = reqwest::Client::builder();
+//! // Customize HTTP client with `http_client_builder` here
+//!
+//! let client = api::v2::Client::builder()
+//!     .http_client(http_client_builder.build().unwrap())
+//!     .build();
+//! ```
+//!
+//! # Crate status
+//!
+//! Currently, this crate is a bit minimalistic and does not implement all the [Exercism](https://exercism.org)
+//! API endpoints. To suggest new endpoints to add, you can enter an [issue](https://github.com/clechasseur/mini_exercism/issues).
+//! Or, even better, don't hesitate to submit a [pull request](https://github.com/clechasseur/mini_exercism/pulls)! 😁
+//!
+//! # Minimum Rust version
+//!
+//! TODO: complete this section
+//!
+//! [`Credentials`]: core::Credentials
 
 pub mod api;
 #[cfg(feature = "cli")]
