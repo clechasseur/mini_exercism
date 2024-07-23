3fdb62f41b5e69dc07251b61119730881e9cd569
Charles Lechasseur <shiftingbeard@outlook.com>
2024-07-23 01:23:05 -0400

    fix: honor `--check-cfg` for Rust 1.80.0 and Nightly

--- build.rs
+++ build.rs
@@ -6,11 +6,13 @@ use rustc_version::Channel::Nightly;
 fn main() {
     println!("cargo:rerun-if-env-changed=CI");
 
+    println!("cargo:rustc-check-cfg=cfg(nightly_rustc)");
     if version_meta().unwrap().channel <= Nightly {
         println!("cargo:rustc-cfg=nightly_rustc");
     }
 
     // On CI, do not run tests using the real endpoints.
+    println!("cargo:rustc-check-cfg=cfg(skip_tests_with_real_endpoints)");
     if env::var("CI").is_ok() {
         println!("cargo:rustc-cfg=skip_tests_with_real_endpoints")
     }
