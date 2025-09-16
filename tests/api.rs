8496d5990b3a329424284d2b543e53e2a9f5836c
Charles Lechasseur <shiftingbeard@outlook.com>
2025-09-16 00:36:35 -0400

    chore: get rid of build script

--- tests/api.rs
+++ tests/api.rs
@@ -1,4 +1,4 @@
-#[cfg(all(not(skip_tests_with_real_endpoints), not(tarpaulin), not(coverage)))]
+#[cfg(all(mini_exercism_enable_tests_with_real_endpoints, not(tarpaulin), not(coverage)))]
 mod real_endpoints;
 mod v1;
 mod v2;
